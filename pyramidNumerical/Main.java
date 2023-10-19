3236e1365dc76453b847976050dd86c064cbeb8f
Zedgamer9128 <33319711+zedgamer9128@users.noreply.github.com>
2023-10-19 22:10:14 +0530

    Modified pyramidNumerical

--- pyramidNumerical/Main.java
+++ pyramidNumerical/Main.java
@@ -5,12 +5,15 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter Row: ");
     int r = sc.nextInt();
-    for (int i = 0; i < r; i++) {
-      for (int j = 0; j < r - (i + 1); j++) {
+    for (int i = 1; i <= r; i++) {
+      for (int j = 1; j <= r - i; j++) {
         System.out.print(" ");
       }
-      for (int j = 0; j < (2 * (i + 1)) - 1; j++) {
-        System.out.print(j + 1);
+      for (int j = 1; j <= i; j++) {
+        System.out.print(j);
+      }
+      for (int j = i - 1; j >= 1; j--) {
+        System.out.print(j);
       }
       System.out.println();
     }
