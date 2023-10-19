4eb7ba82769395cfac9aca9de26a147f173e3ee6
Zedgamer9128 <33319711+zedgamer9128@users.noreply.github.com>
2023-10-19 18:46:28 +0530

    Complete pascal

--- pascal/Main.java
+++ pascal/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import zed.array.Display;
 
 public class Main {
   public static void main(String[] args) {
@@ -7,10 +8,13 @@ public class Main {
     int r = sc.nextInt();
     int[][] a = new int[r][];
     for (int i = 0; i < r; i++) {
+      a[i] = new int[i + 1];
       a[i][0] = a[i][i] = 1;
       for (int j = 1; j < i; j++) {
         a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
       }
     }
+    Display p = new Display();
+    p.print(a);
   }
 }
