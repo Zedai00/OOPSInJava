402322162475d98731928597a715ee61ab81db47
Zedgamer9128 <33319711+zedgamer9128@users.noreply.github.com>
2023-10-23 00:59:15 +0530

    Add swapTwoValues

---
+++ swapTwoValues/usingTemporaryVariable/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  static void swapTwoValues(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    System.out.println("After Swapping");
+    System.out.printf("A: %d and B: %d\n", a, b);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter A: ");
+    int a = sc.nextInt();
+    System.out.print("Enter B: ");
+    int b = sc.nextInt();
+    System.out.println("Before Swapping");
+    System.out.printf("A: %d and B: %d\n", a, b);
+    swapTwoValues(a, b);
+  }
+}
